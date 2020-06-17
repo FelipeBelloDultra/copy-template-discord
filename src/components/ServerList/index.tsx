a55f0e52ab565d1b681c048136e20587922875e6
FelipeBelloDultra <felipe_bello_dultra@hotmail.com>
2020-06-17 20:14:21 -0300

    Server List pronto

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { Container, Separator } from './styles';
+import ServerButton from '../ServerButton';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={6} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  );
+}
+
+export default ServerList;
